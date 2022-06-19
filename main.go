19fb3e700c3c2bd2a27cbe0b0d2dd548e9765450
Eitan Nargassi <eitan1112@gmail.com>
2022-06-19 21:10:45 +0300

    updates

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"terraform-provider-nyno/terraform-provider-nyno/nyno"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
-	"terraform-provider-nyno/nyno"
+	"github.com/nyno-app/terraform-provider-nyno/internal/provider"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
-			return nyno.Provider()
+			return provider.Provider()
 		},
 	})
 }
