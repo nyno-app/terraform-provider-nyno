187ac6038f2eaa85eb6d4a5eac3b5705b74acb4b
Eitan Nargassi <eitan1112@gmail.com>
2022-06-24 14:14:47 +0300

    added authentication

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -2,9 +2,13 @@
 # Run with examples/apply.sh
 
 cd internal/provider
+pwd
+echo "Building"
 go build -o terraform-provider-nyno
+echo "Making"
+cd ../..
 make install
-cd ../../scripts
+cd scripts
 rm -rf ".terraform"
 rm -f ".terraform.lock.hcl" "terraform.tfstate"
 terraform init
--- scripts/main.tf
+++ scripts/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     nyno = {
-      version = "1.0.0"
+      version = "1.1.1"
       source  = "nyno.io/nyno/nyno"
     }
   }
@@ -9,6 +9,9 @@ terraform {
 
 provider "nyno" {
   api_endpoint = "http://localhost:3000/api"
+  organization = "customer"
+  username     = "someuser"
+  password     = "123456789123456789"
 }
 
 resource "nyno_template" "new" {
@@ -27,15 +30,6 @@ resource "nyno_template" "new" {
     type     = "string"
   }
 
-  action {
-    type          = "createFile"
-    source_branch = "yoyoy"
-    target_branch = "efeojf"
-    path          = "string"
-    template_code = filebase64("templatefile.yaml")
-    pull_request  = false
-    repository_id = "4fbf3dc3-2a66-41f3-87b7-70c7ec9f9609"
-  }
 
   action {
     type          = "createFile2"
@@ -60,6 +54,7 @@ resource "nyno_role" "new" {
   delete_repository                = true
   get_user                         = true
   update_user                      = true
+  create_user                      = true
   get_role                         = false
   update_role                      = true
   create_role                      = true
@@ -72,16 +67,18 @@ resource "nyno_role" "new" {
   update_all_deployments           = true
   create_deployments_all_templates = true
   delete_all_deployments           = true
+  get_global_settings              = true
+  update_global_settings           = true
 }
 
 data "nyno_template" "template" {
-  id = "087b10de-d672-4b88-8c84-14432521e856"
+  id = "45a73a32-7ac9-4639-b0a7-33c1ea840244"
 }
 
 data "nyno_role" "my_role" {
-  id = "5ec2219f-4e65-43d8-aa4f-ba880c1e436e"
+  id = "9bf21461-eb62-4289-a4be-e446a495d20f"
 }
 
 data "nyno_repository" "my_rpository" {
-  url = "https://github.com/Eitan1112/crypto-trader"
+  url = "https://github.com/Eitan1112/chat-app"
 }
